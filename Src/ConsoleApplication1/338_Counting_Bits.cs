e0011fd828542797431a8f775092ba915a90ad12
LouisGuo <2213429531@qq.com>
2016-08-11 17:00:11 +0800

    init code

---
+++ Src/ConsoleApplication1/338_Counting_Bits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Counting_Bits
+    {
+        public int[] Sollution(int num)
+        {
+            var result = new int[num];
+
+            return result;
+        }
+    }
+}
---
+++ Src/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tool = new Counting_Bits();
+
+            for (int i = 0; i < Math.Pow(2, 5); i++)
+            {
+                Console.WriteLine(String.Join(", ", tool.Sollution(i)));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
