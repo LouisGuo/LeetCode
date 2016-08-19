3f333a597bad0728773d23caf00739322bfa1d78
LouisGuo <2213429531@qq.com>
2016-08-19 17:27:23 +0800

    ExcelSheetColumnTitle

---
+++ Src/LeetCode/168_Excel_Sheet_Column_Title.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ExcelSheetColumnTitle
+    {
+        public string Sollution(int n)
+        {
+            if (n < 0)
+                return String.Empty;
+            var result = String.Empty;
+            n = n - 1;
+            while (n !=0)
+            {
+                result = (Char)(n % 26 + 65) + result;
+                n = n / 26;
+            }
+            return result;
+        }
+    }
+}
