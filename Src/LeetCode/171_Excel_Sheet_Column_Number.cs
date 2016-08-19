42aa1c99aa5902b51bf412c03f68d0dac11567f7
LouisGuo <2213429531@qq.com>
2016-08-19 17:01:26 +0800

    ExcelSheetColumnNumber

---
+++ Src/LeetCode/171_Excel_Sheet_Column_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ExcelSheetColumnNumber
+    {
+        public int Sollution(string s)
+        {
+            if (s == null || s.Length == 0)
+                return 0;
+            var result = 0;
+            var level = 1;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                result = result + level * ((int)s[i] - 64);
+                level *= 26;
+            }
+            return result;
+        }
+    }
+}
