b5f3cf229ecdd6900a66bfde7443c8c42816583e
LouisGuo <2213429531@qq.com>
2016-08-19 16:37:44 +0800

    RansomNote

---
+++ Src/LeetCode/383_Ransom_Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RansomNote
+    {
+        public bool Sollution(string ransomNote, string magazine)
+        {
+            var sb = new StringBuilder(magazine);
+            foreach (var ch in ransomNote)
+            {
+                var contains = false;
+                for (int i = 0; i < sb.Length; i++)
+                {
+                    if (sb[i].Equals(ch))
+                    {
+                        sb.Remove(i, 1);
+                        contains = true;
+                    }
+                }
+                if (!contains)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
