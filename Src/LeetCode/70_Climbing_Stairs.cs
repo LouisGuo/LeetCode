51039b304dbd7674a12f351a33b4c5337d6bc45b
LouisGuo <2213429531@qq.com>
2016-08-20 14:14:39 +0800

    ClimbingStairs

---
+++ Src/LeetCode/70_Climbing_Stairs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ClimbingStairs
+    {
+        public int Sollution(int n)
+        {
+            if (n < 3)
+                return n;
+            return Sollution(n - 1) + Sollution(n - 2);
+        }
+
+        public int Sollution_Fast(int n)
+        {
+            if (n < 3)
+                return n;
+            var fn_2 = 1;
+            var fn_1 = 2;
+            for (int i = 4; i <= n; i++)
+            {
+                var temp = fn_2 + fn_1;
+                fn_2 = fn_1;
+                fn_1 = temp;
+            }
+            return fn_1 + fn_2;
+        }
+    }
+}
