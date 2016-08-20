a4c8628415ec086f351c0e2beda086fa8c376d54
LouisGuo <2213429531@qq.com>
2016-08-20 14:39:29 +0800

    SwapNodesinPairs

---
+++ Src/LeetCode/24_Swap_Nodes_in_Pairs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SwapNodesinPairs
+    {
+        public ListNode Sollution(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            var current = head;
+            head = head.next;
+            current.next = head.next;
+            head.next = current;
+            var temp = default(ListNode);
+            while (current.next != null && current.next.next != null)
+            {
+                temp = current.next;
+                current.next = current.next.next;
+                temp.next = temp.next.next;
+                current.next.next = temp;
+                current = current.next.next;
+            }
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
