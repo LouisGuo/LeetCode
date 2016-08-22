e9d5b1c37c96b55bb38e4ea192332447195e3602
LouisGuo <2213429531@qq.com>
2016-08-22 09:15:54 +0800

    RemoveElement

---
+++ Src/LeetCode/27_Remove_Element.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveElement
+    {
+        public int Sollution(int[] nums, int val)
+        {
+            var head = 0;
+            var tail = nums.Length - 1;
+            while (head <= tail)
+            {
+                while (head < nums.Length && nums[head] != val)
+                {
+                    head++;
+                }
+                while (tail > -1 && nums[tail] == val)
+                {
+                    tail--;
+                }
+                if (head < tail)
+                {
+                    var temp = nums[tail];
+                    nums[tail] = nums[head];
+                    nums[head] = temp;
+                }
+            }
+            return head;
+        }
+    }
+}
