8ae0487821d1ac85074664656e259bce21d2e2c6
LouisGuo <2213429531@qq.com>
2016-08-22 11:06:42 +0800

    LongestCommonPrefix

---
+++ Src/LeetCode/14_Longest_Common_Prefix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestCommonPrefix
+    {
+        public string Sollution(string[] strs)
+        {
+            var commonPrefix = String.Empty;
+            if (strs != null && strs.Length > 0)
+            {
+                for (int j = 0; j < strs[0].Length; j++)
+                {
+                    var ch = strs[0][j];
+                    var isCommon = true;
+                    for (int i = 0; i < strs.Length; i++)
+                    {
+                        if (strs[i] != null && strs[i].Length > j && strs[i][j].Equals(ch))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            isCommon = false;
+                            break;
+                        }
+                    }
+                    if (isCommon)
+                    {
+                        commonPrefix += ch;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return commonPrefix;
+        }
+    }
+}
