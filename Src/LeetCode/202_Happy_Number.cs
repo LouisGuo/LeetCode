aa326a18aacd8a2bb595f2229b85cd4dcfcadab6
LouisGuo <2213429531@qq.com>
2016-08-22 11:45:31 +0800

    IsHappy

---
+++ Src/LeetCode/202_Happy_Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            var middleValues = new List<int>();
+            while (!middleValues.Contains(n) && n != 1)
+            {
+                middleValues.Add(n);
+                var middleValue = 0;
+                while (n != 0)
+                {
+                    var tem = n % 10;
+                    middleValue += (tem * tem);
+                    n /= 10;
+                }
+                n = middleValue;
+            }
+            return n == 1;
+        }
+    }
+}
