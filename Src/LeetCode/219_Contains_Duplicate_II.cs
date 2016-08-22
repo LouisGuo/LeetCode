d432f7ad8158db3264ae30b6bdd3c34324ea3ae9
LouisGuo <2213429531@qq.com>
2016-08-22 10:36:44 +0800

    ContainsDuplicateII

--- Src/LeetCode/219_Contains_Duplicate_II.cs
+++ Src/LeetCode/219_Contains_Duplicate_II.cs
@@ -26,5 +26,25 @@ namespace LeetCode
             }
             return false;
         }
+
+        public bool Sollution_Fast(int[] nums, int k)
+        {
+            if (nums.Length > 1)
+            {
+                var lastIndexs = new Dictionary<int, int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (lastIndexs.ContainsKey(nums[i]) && lastIndexs[nums[i]] + k >= i)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        lastIndexs[nums[i]] = i;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
