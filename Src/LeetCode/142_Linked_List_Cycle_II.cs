08ecf7937f711784aaa8dc01023b04634b2561fa
LouisGuo <2213429531@qq.com>
2016-08-22 17:35:55 +0800

    LinkedListCycleII

---
+++ Src/LeetCode/142_Linked_List_Cycle_II.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LinkedListCycleII
+    {
+        public ListNode Sollution(ListNode head)
+        {
+            if (head != null)
+            {
+                var set = new HashSet<ListNode>();
+                var current = head;
+                while (current != null)
+                {
+                    if (set.Contains(current))
+                    {
+                        return current;
+                    }
+                    set.Add(current);
+                    current = current.next;
+                }
+            }
+            return null;
+        }
+
+        public ListNode Sollution_Fast(ListNode head)
+        {
+            if (head != null)
+            {
+                if (head.next == head)
+                    return head;
+                var current = head;
+                var pre = default(ListNode);
+                while (IsCycle(current))
+                {
+                    pre = current;
+                    current = current.next;
+                    pre.next = null;
+                }
+                if (current != head)
+                {
+                    pre.next = current;
+                    return pre;
+                }
+            }
+            return null;
+        }
+
+        public bool IsCycle(ListNode head)
+        {
+            if (head == null)
+                return false;
+            var slow = head;
+            var fast = head.next;
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null)
+                    return false;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+    }
+}
---
+++ Src/Test/LinkedListCycleIITests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static LeetCode.LinkedListCycleII;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class LinkedListCycleIITests
+    {
+        [TestMethod()]
+        public void Sollution_FastTest()
+        {
+            var listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(3);
+            var node = new ListNode(4);
+            listNode.next.next.next = node;
+            listNode.next.next.next.next = new ListNode(5);
+            listNode.next.next.next.next.next = new ListNode(6);
+            listNode.next.next.next.next.next.next = new ListNode(7);
+            listNode.next.next.next.next.next.next.next = new ListNode(8);
+            listNode.next.next.next.next.next.next.next.next = new ListNode(9);
+            listNode.next.next.next.next.next.next.next.next.next = new ListNode(10);
+            listNode.next.next.next.next.next.next.next.next.next.next = new ListNode(11);
+            var tail = new ListNode(12);
+            listNode.next.next.next.next.next.next.next.next.next.next.next = tail;
+            tail.next = node;
+            var tool = new LinkedListCycleII();
+            var result = tool.Sollution_Fast(listNode);
+        }
+
+        [TestMethod()]
+        public void Sollution_FastTest1()
+        {
+            var listNode = new ListNode(1);
+            listNode.next = listNode;
+            var tool = new LinkedListCycleII();
+            var result = tool.Sollution_Fast(listNode);
+        }
+
+        [TestMethod()]
+        public void Sollution_FastTest2()
+        {
+            var listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(3);
+            var node = new ListNode(4);
+            listNode.next.next.next = node;
+            listNode.next.next.next.next = new ListNode(5);
+            listNode.next.next.next.next.next = new ListNode(6);
+            listNode.next.next.next.next.next.next = new ListNode(7);
+            listNode.next.next.next.next.next.next.next = new ListNode(8);
+            listNode.next.next.next.next.next.next.next.next = new ListNode(9);
+            listNode.next.next.next.next.next.next.next.next.next = new ListNode(10);
+            listNode.next.next.next.next.next.next.next.next.next.next = new ListNode(11);
+            var tail = new ListNode(12);
+            listNode.next.next.next.next.next.next.next.next.next.next.next = tail; 
+            var tool = new LinkedListCycleII();
+            var result = tool.Sollution_Fast(listNode);
+        }
+    }
+}
