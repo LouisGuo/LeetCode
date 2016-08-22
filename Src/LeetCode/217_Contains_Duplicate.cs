f6b779b29a158befa95299fcfa6d5931a05fb42c
LouisGuo <2213429531@qq.com>
2016-08-22 13:51:34 +0800

    ContainsDuplicate

---
+++ Src/LeetCode/217_Contains_Duplicate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainsDuplicate
+    {
+        public bool Sollution(int[] nums)
+        {
+            if (nums != null && nums.Length > 1)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] == nums[j])
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool Sollution_Fast(int[] nums)
+        {
+            return nums != null && nums.Length != nums.Distinct().Count();
+        }
+
+        public bool Sollution_Faster(int[] nums)
+        {
+            if (nums != null && nums.Length > 1)
+            {
+                var min = 0;
+                var max = 0;
+                foreach (var num in nums)
+                {
+                    if (num < min)
+                        min = num;
+                    else if (num > max)
+                        max = num;
+                }
+                var bitArray = new BitArray(max - min + 1);
+                foreach (var num in nums)
+                {
+                    var index = num - min;
+                    if (bitArray[index])
+                        return true;
+                    else
+                        bitArray[index] = true;
+                }
+            }
+            return false;
+        }
+
+        public bool Sollution_FasterEvenMore(int[] nums)
+        {
+            if (nums != null && nums.Length > 1)
+            {
+                var min = 0;
+                var max = 0;
+                foreach (var num in nums)
+                {
+                    if (num < min)
+                        min = num;
+                    else if (num > max)
+                        max = num;
+                }
+                var bitArray = new BitArray(max - min + 1);
+                if (bitArray.Length >= nums.Length)
+                {
+                    foreach (var num in nums)
+                    {
+                        var index = num - min;
+                        if (bitArray[index])
+                            return true;
+                        else
+                            bitArray[index] = true;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
