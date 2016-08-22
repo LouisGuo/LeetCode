b73c2fff4e9cc85327e5744f75d97b8b84942a48
LouisGuo <2213429531@qq.com>
2016-08-22 11:30:59 +0800

    TwoSum

---
+++ Src/LeetCode/1_Two_Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class TwoSum
+    {
+        public int[] Sollution(int[] nums, int target)
+        {
+            var result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+                }
+            }
+            return result;
+        }
+    }
+}
