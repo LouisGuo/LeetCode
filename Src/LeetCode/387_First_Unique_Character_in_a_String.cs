db4510a7e9fe2461d52f538acf33712b43a29837
LouisGuo <2213429531@qq.com>
2016-08-22 16:20:31 +0800

    FirstUniqueCharacterinaString

---
+++ Src/LeetCode/387_First_Unique_Character_in_a_String.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class FirstUniqueCharacterinaString
+    {
+        public int Sollution(string s)
+        {
+            if (s != null)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    bool isUnique = true;
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        if (i != j && s[i].Equals(s[j]))
+                        {
+                            isUnique = false;
+                            break;
+                        }
+                    }
+                    if (isUnique)
+                        return i;
+                }
+            }
+            return -1;
+        }
+
+        public int Sollution_Fast(string s)
+        {
+            if (s != null && !s.Equals(String.Empty))
+            {
+                var charConut = new int[Char.MaxValue];
+                foreach (int ch in s)
+                {
+                    charConut[ch]++;
+                }
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (charConut[s[i]] == 1)
+                        return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
