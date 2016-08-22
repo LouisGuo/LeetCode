837a7208b867ca56a05ce809ce8e165af74cc3be
LouisGuo <2213429531@qq.com>
2016-08-22 14:11:23 +0800

    ValidAnagram

---
+++ Src/LeetCode/242_Valid_Anagram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ValidAnagram
+    {
+        public bool Sollution(string s, string t)
+        {
+            if (s != null && t != null && s.Length == t.Length)
+            {
+                var charDic = new Dictionary<char, int>();
+                foreach (var ch in s)
+                {
+                    if (charDic.ContainsKey(ch))
+                        charDic[ch]++;
+                    else
+                        charDic[ch] = 1;
+                }
+                foreach (var ch in t)
+                {
+                    if (charDic.ContainsKey(ch))
+                    {
+                        charDic[ch]--;
+                        if (charDic[ch] < 0)
+                            return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
