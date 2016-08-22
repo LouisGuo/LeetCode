00c62622a3ef5200a4c52524f7f3cfceb9fc7f50
LouisGuo <2213429531@qq.com>
2016-08-22 10:44:56 +0800

    LengthofLastWord

---
+++ Src/LeetCode/58_Length_of_Last_Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LengthofLastWord
+    {
+        public int Sollution(string s)
+        {
+            var result = 0;
+            if (s != null && !s.Equals(String.Empty))
+            {
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    if (s[i].Equals(' ') && result == 0)
+                        continue;
+                    else if (!s[i].Equals(' '))
+                        result++;
+                    else
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
