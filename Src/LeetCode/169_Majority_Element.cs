5d8010d9dc85e343e0a620a0638c40a13c4c8d9c
LouisGuo <2213429531@qq.com>
2016-08-22 14:02:18 +0800

    MajorityElement

---
+++ Src/LeetCode/169_Majority_Element.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MajorityElement
+    {
+        public int Sollution(int[] nums)
+        {
+            var countDic = new Dictionary<int, int>();
+            var lessCount = nums.Length / 2;
+            if (nums != null)
+            {
+                if (nums.Length == 1)
+                {
+                    return nums[0];
+                }
+                else
+                {
+                    foreach (var num in nums)
+                    {
+                        if (countDic.ContainsKey(num))
+                        {
+                            countDic[num]++;
+                            if (countDic[num] > lessCount)
+                                return num;
+                        }
+                        else
+                            countDic[num] = 1;
+                    }
+                }
+            }
+            throw new Exception();
+        }
+    }
+}
