9ff282a61eb3d3dd588b2f0f23bc27a4d3675a85
LouisGuo <2213429531@qq.com>
2016-08-22 09:24:16 +0800

    RemoveLinkedListElements

---
+++ Src/LeetCode/203_Remove_Linked_List_Elements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveLinkedListElements
+    {
+        public ListNode Sollution(ListNode head, int val)
+        {
+            while (head != null && head.val == val)
+            {
+                var temp = head;
+                head = head.next;
+                temp.next = null;
+            }
+            var current = head;
+            while (current != null && current.next != null)
+            {
+                if (current.next.val == val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            }
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
