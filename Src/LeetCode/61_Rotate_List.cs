e15644faf05b579f2b4c542588ccbe8d669edf63
LouisGuo <2213429531@qq.com>
2016-08-24 22:18:56 +0800

    RotateList

---
+++ Src/LeetCode/61_Rotate_List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RotateList
+    {
+        public ListNode Sollution(ListNode head, int k)
+        {
+
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+    }
+}
