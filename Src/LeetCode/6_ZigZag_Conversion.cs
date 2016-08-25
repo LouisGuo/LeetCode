43c26719ef45c982b9055b720f1b34b86454565c
LouisGuo <2213429531@qq.com>
2016-08-25 18:38:32 +0800

    ZigZagConversion

---
+++ Src/LeetCode/6_ZigZag_Conversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ZigZagConversion
+    {
+        public string Sollution(string s, int numRows)
+        {
+            if (s != null && s.Length > 2 && numRows > 1)
+            {
+                var result = new StringBuilder();
+                for (int i = 0; i < numRows; i++)
+                {
+                    
+                }
+                return result.ToString();
+            }
+            else
+            {
+                return s;
+            }
+        }
+    }
+}
