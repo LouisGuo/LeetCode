329ffc233a0cd69c01d702b4a3930ef6b2825647
LouisGuo <2213429531@qq.com>
2016-08-25 21:52:25 +0800

    LinkedListRandomNode 蓄水池取样

---
+++ Src/LeetCode/LinkedList/382_Linked_List_Random_Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LinkedListRandomNode
+    {
+        private ListNode head;
+
+        public LinkedListRandomNode(ListNode head)
+        {
+            this.head = head;
+        }
+
+        public ListNode Sollution()
+        {
+            var result = head;
+            var current = head.next;
+            var count = 1;
+            var random = new Random();
+            while (current != null)
+            {
+                if (random.Next(++count) == 0)
+                {
+                    result = current;
+                }
+                current = current.next;
+            }
+            return result;
+        }
+    }
+}
---
+++ Src/Test/LinkedList/LinkedListRandomNodeTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class LinkedListRandomNodeTests
+    {
+        [TestMethod()]
+        public void SollutionTest()
+        {
+            var head = new ListNode(1);
+            var current = head;
+            for (int i = 2; i <= 100; i++)
+            {
+                current.next = new ListNode(i);
+                current = current.next;
+            }
+            var tool = new LinkedListRandomNode(head);
+            var results = new List<int>();
+            for (int i = 0; i < 100000; i++)
+            {
+                results.Add(tool.Sollution().val);
+                System.Threading.Thread.Sleep(123);
+            }
+            var re = results.GroupBy(t => t)
+                .Select(i => new KeyValuePair<int, int>(i.Key, i.Count()))
+                .OrderBy(i => i.Key).ToList();
+
+        }
+    }
+}
