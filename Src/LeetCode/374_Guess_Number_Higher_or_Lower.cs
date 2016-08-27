d3d128b30235771807c8edd008fa58752e513d8f
LouisGuo <2213429531@qq.com>
2016-08-27 13:06:37 +0800

    GuessNumberHigherorLower

---
+++ Src/LeetCode/374_Guess_Number_Higher_or_Lower.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class GuessNumberHigherorLower
+    {
+        public int Sollution(int n)
+        {
+            var left = 1;
+            var right = n;
+            var middle = left + (right - left) / 2;
+            while (left < right - 1)
+            {
+                var guessResult = guess(middle);
+                if (guessResult == 1)
+                {
+                    right = middle;
+                }
+                else if (guessResult == -1)
+                {
+                    left = middle;
+                }
+                else
+                {
+                    return middle;
+                }
+                middle = left + (right - left) / 2;
+            }
+            return guess(left) == 0 ? left : right;
+        }
+
+        private int guess(int num)
+        {
+            var result = num - 6;
+            if (result > 0)
+                return 1;
+            else if (result < 0)
+                return -1;
+            return 0;
+        }
+    }
+}
---
+++ Src/Test/GuessNumberHigherorLowerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class GuessNumberHigherorLowerTests
+    {
+        [TestMethod()]
+        public void SollutionTest()
+        {
+            var tool = new GuessNumberHigherorLower();
+            var result = tool.Sollution(10);
+        }
+    }
+}
