5587e3d43505e27fa6772e401628560b6398d715
LouisGuo <2213429531@qq.com>
2016-08-28 16:59:31 +0800

    MaximalRectangle

---
+++ Src/LeetCode/85_Maximal_Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MaximalRectangle
+    {
+        public int Sollution(char[,] matrix)
+        {
+            var result = 0;
+            var x = matrix.GetLength(0);
+            var y = matrix.GetLength(1);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    var startX = i;
+                    var startY = j; 
+                    for (int tempX = startX; tempX < x; tempX++)
+                    {
+                        for (int tempY = startY; tempY < y; tempY++)
+                        {
+                            var currentCount = (tempX - startX + 1) * (tempY - startY + 1);
+                            if (currentCount > result && IsValid(matrix, startX, startY, tempX, tempY))
+                                result = currentCount;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private bool IsValid(char[,] matrix, int startX, int startY, int endX, int endY)
+        {
+            for (int i = startX; i <= endX; i++)
+            {
+                for (int j = startY; j <= endY; j++)
+                {
+                    if (matrix[i, j] == '0')
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
