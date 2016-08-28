f82bc8ee25de752de9de57285dc4c844df7049ed
LouisGuo <2213429531@qq.com>
2016-08-28 12:59:19 +0800

    RectangleArea

---
+++ Src/LeetCode/223_Rectangle_Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RectangleArea
+    {
+        public int Sollution(int A, int B, int C, int D, int E, int F, int G, int H)
+        {
+            var a = Math.Max(A, E);
+            var b = Math.Max(B, F);
+            var c = Math.Min(C, G);
+            var d = Math.Min(D, H);
+            var total = (C - A) * (D - B) + (G - E) * (H - F);
+            if (c > a && d > b)
+                total -= (c - a) * (d - b);
+            return total;
+        }
+    }
+}
