e615104b66e75da690b8140a472f6418334ab0c6
LouisGuo <2213429531@qq.com>
2016-08-29 20:23:53 +0800

    SingleNumber

---
+++ Src/LeetCode/136_Single_Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SingleNumber
+    {
+        public int Solution(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            var result = 0;
+            foreach (var num in nums)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
