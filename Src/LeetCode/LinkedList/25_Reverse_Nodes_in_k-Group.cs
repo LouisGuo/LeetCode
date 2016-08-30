8e19ecbc24be9691fceb9ceace04a59f0a249461
LouisGuo <2213429531@qq.com>
2016-08-30 21:52:42 +0800

    ReverseNodesinkGroup

---
+++ Src/LeetCode/LinkedList/25_Reverse_Nodes_in_k-Group.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ReverseNodesinkGroup
+    {
+        public ListNode Solution(ListNode head, int k)
+        {
+            if (head == null || head.next == null || k < 2)
+            {
+                return head;
+            }
+            var rest = head;
+            var tail = head;
+            while (rest != null)
+            {
+                int cursor = 0;
+                var currentStart = rest;
+                for (cursor = 0; cursor < k && rest != null; cursor++)
+                {
+                    rest = rest.next;
+                }
+                if (cursor == k)
+                {
+                    var restTemp = currentStart.next;
+                    var headTemp = currentStart;
+                    while (rest != restTemp)
+                    {
+                        var temp = restTemp.next;
+                        restTemp.next = headTemp;
+                        headTemp = restTemp;
+                        restTemp = temp;
+                    }
+                    if (tail == head)
+                    {
+                        head = headTemp;
+                    }
+                    else
+                    {
+                        tail.next = headTemp;
+                    }
+                    tail = currentStart;
+                    tail.next = rest;
+                }
+                else
+                {
+                    if (tail != head)
+                        tail.next = currentStart;
+                }
+            }
+            return head;
+        }
+    }
+}
