95e2cc333c5c913f3f339437e317ae1059004044
LouisGuo <2213429531@qq.com>
2016-08-30 14:41:49 +0800

    OddEvenLinkedList

---
+++ Src/LeetCode/LinkedList/328_Odd_Even_Linked_List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class OddEvenLinkedList
+    {
+        public ListNode Solution(ListNode head)
+        {
+            if (head != null && head.next != null)
+            {
+                var odd = head;
+                var even = head.next;
+                var oddTail = odd;
+                var evenTail = even;
+                var oddCurrent = odd;
+                var evenCurrent = even;
+                oddCurrent = evenCurrent.next;
+                evenCurrent = oddCurrent == null ? null : oddCurrent.next;
+                while (oddCurrent != null || evenCurrent != null)
+                {
+                    oddTail.next = oddCurrent;
+                    evenTail.next = evenCurrent;
+                    if (oddCurrent != null)
+                    {
+                        oddTail = oddCurrent;
+                    }
+                    if (evenCurrent != null)
+                    {
+                        evenTail = evenCurrent;
+                    }
+                    oddCurrent = evenCurrent == null ? null : evenCurrent.next;
+                    evenCurrent = oddCurrent == null ? null : oddCurrent.next;
+                }
+                oddTail.next = even;
+            }
+            return head;
+        }
+    }
+}
