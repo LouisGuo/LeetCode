99916e011864cb449a0e4f1bc042fa6891f6912f
LouisGuo <2213429531@qq.com>
2016-08-30 20:47:27 +0800

    RemoveDuplicatesfromSortedArray

---
+++ Src/LeetCode/26_Remove_Duplicates_from_Sorted_Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveDuplicatesfromSortedArray
+    {
+        public int Solution(int[] nums)
+        {
+            var current = -1;
+            if (nums != null && nums.Length > 0)
+            {
+                current = 0;
+                var cursor = 1;
+                while (cursor < nums.Length)
+                {
+                    if (nums[cursor] == nums[current])
+                        cursor++;
+                    else
+                    {
+                        nums[++current] = nums[cursor++];
+                    }
+                }
+            }
+            return current + 1;
+        }
+    }
+}
