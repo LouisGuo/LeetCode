0a54ffb5f1773211c3e9f9b180ea88ee8f23f159
LouisGuo <2213429531@qq.com>
2016-08-31 19:55:24 +0800

    RemoveDuplicatesfromSortedArrayII

---
+++ Src/LeetCode/80_Remove_Duplicates_from_Sorted_Array_II.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveDuplicatesfromSortedArrayII
+    {
+        public int Solution(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            else if (nums.Length < 3)
+                return nums.Length;
+            else
+            {
+                var result = 0;
+                var hasTwo = false;
+                for (int current = 1; current < nums.Length; current++)
+                {
+                    if (nums[current] == nums[result])
+                    {
+                        if (!hasTwo)
+                        {
+                            hasTwo = true;
+                            nums[++result] = nums[current];
+                        }
+                    }
+                    else
+                    {
+                        hasTwo = false;
+                        nums[++result] = nums[current];
+                    }
+                }
+                return ++result;
+            }
+        }
+    }
+}
