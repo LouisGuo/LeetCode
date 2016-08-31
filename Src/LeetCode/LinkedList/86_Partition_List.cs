138d04f8a9981fa5b39d1ddf9f31c9361f88fd80
LouisGuo <2213429531@qq.com>
2016-08-31 20:38:15 +0800

    PartitionList

---
+++ Src/LeetCode/LinkedList/86_Partition_List.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LinkedList
+{
+    public class PartitionList
+    {
+        public ListNode Solution(ListNode head, int x)
+        {
+            if (head == null || head.next == null)
+                return head;
+            var left = default(ListNode);
+            var right = default(ListNode);
+            var leftTail = default(ListNode);
+            var rightTail = default(ListNode);
+            while (head != null)
+            {
+                if (head.val < x)
+                {
+                    if (left == null)
+                    {
+                        left = head;
+                        leftTail = head;
+                    }
+                    else
+                    {
+                        leftTail.next = head;
+                        leftTail = leftTail.next;
+                    }
+                }
+                else
+                {
+                    if (right == null)
+                    {
+                        right = head;
+                        rightTail = head;
+                    }
+                    else
+                    {
+                        rightTail.next = head;
+                        rightTail = rightTail.next;
+                    }
+                }
+                head = head.next;
+            }
+            if (left == null)
+                left = right;
+            else
+                leftTail.next = right;
+            if (right != null)
+                rightTail.next = null;
+            return left;
+        }
+    }
+}
