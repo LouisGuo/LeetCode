f55ef70eac00c5d4fb9ef6b91d356b40ed1a26b9
LouisGuo <2213429531@qq.com>
2016-08-31 17:30:57 +0800

    MergeIntervals draft

---
+++ Src/LeetCode/56_Merge_Intervals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeIntervals
+    {
+        public IList<Interval> Solution(IList<Interval> intervals)
+        {
+            var result = new List<Interval>();
+            if (intervals != null && intervals.Count > 0)
+            {
+                result.Add(intervals[0]);
+                for (int i = 1; i < intervals.Count; i++)
+                {
+                    
+
+                }
+            }
+            return result;
+        }
+
+        public class Interval
+        {
+            public int start;
+            public int end;
+            public Interval() { start = 0; end = 0; }
+            public Interval(int s, int e) { start = s; end = e; }
+        }
+    }
+}
