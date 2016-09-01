e24e965bafd8672d8b06dcb6ea9931587c1c3c8b
LouisGuo <2213429531@qq.com>
2016-09-01 14:38:12 +0800

    MissingNumber

---
+++ Src/LeetCode/268_Missing_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MissingNumber
+    {
+        public int Solution(int[] nums)
+        {
+            if (nums != null && nums.Length > 0)
+            {
+                var sum = nums.Length;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    sum = sum + i - nums[i];
+                }
+                return sum;
+            }
+            return 0;
+        }
+    }
+}
