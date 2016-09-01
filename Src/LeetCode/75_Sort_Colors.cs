d62f55be404f609ee526e7bcc82a18d2d10a24fb
LouisGuo <2213429531@qq.com>
2016-09-01 13:25:33 +0800

    SortColors

---
+++ Src/LeetCode/75_Sort_Colors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SortColors
+    {
+        public void Solution(int[] nums)
+        {
+            if (nums != null && nums.Length > 1)
+            {
+                var sums = new int[3];
+                foreach (var num in nums)
+                {
+                    sums[num]++;
+                }
+                for (int i = 0; i < sums[0]; i++)
+                {
+                    nums[i] = sums[0];
+                }
+                for (int i = sums[0]; i < sums[1] + sums[0]; i++)
+                {
+                    nums[i] = sums[1];
+                }
+                for (int i = sums[1] + sums[0]; i < nums.Length; i++)
+                {
+                    nums[i] = sums[2];
+                }
+            }
+        }
+    }
+}
