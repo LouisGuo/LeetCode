29a7bbcef58c2a436c910d2572f22ef5cc6c49a3
LouisGuo <2213429531@qq.com>
2016-09-01 14:14:57 +0800

    SuperPow draft

---
+++ Src/LeetCode/372_Super_Pow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SuperPow
+    {
+        public int Solution(int a, int[] b)
+        {
+            if (b != null && b.Length > 0 && a != 0)
+            {
+                var result = 1;
+                var level = 1;
+                for (int i = b.Length - 1; i >= 0; i--)
+                {
+                    var count = b[i] * level;
+                    for (int j = 0; j < count; j++)
+                    {
+                        result *= a;
+                    }
+                    level *= 10;
+                }
+                return result;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Src/Test/SuperPowTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class SuperPowTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var tool = new SuperPow();
+            var result = tool.Solution(int.MaxValue, new int[] { 2,0,0});
+        }
+    }
+}
