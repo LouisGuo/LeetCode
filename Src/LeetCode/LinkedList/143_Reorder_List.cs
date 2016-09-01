101df0361c9b3e307bfd9d8e7e7e9f7f3ca5b9b8
LouisGuo <2213429531@qq.com>
2016-09-01 17:31:04 +0800

    ReorderList draft

---
+++ Src/LeetCode/LinkedList/143_Reorder_List.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ReorderList
+    {
+        public void Solution(ListNode head)
+        {
+            if (head != null && head.next != null)
+            {
+                var stack = new Stack<ListNode>();
+                var queue = new Queue<ListNode>();
+                var current = head;
+                while (current != null)
+                {
+                    queue.Enqueue(current);
+                    current = current.next;
+                    if (current != null)
+                    {
+                        stack.Push(current);
+                        current = current.next;
+                    }
+                }
+                head.next = stack.Pop();
+                current = head.next;
+                while (queue.Count > 0 && stack.Count > 0)
+                {
+                    current.next = queue.Dequeue();
+                    current = current.next;
+                    current.next = stack.Pop();
+                    current = current.next;
+                }
+                if (queue.Count > 0)
+                    current.next = queue.Dequeue();
+                else if (stack.Count > 0)
+                    current.next = stack.Pop();
+                if (current.next != null)
+                    current.next.next = null;
+            }
+        }
+    }
+}
---
+++ Src/Test/LinkedList/ReorderListTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class ReorderListTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var tool = new ReorderList();
+            var sample = new ListNode(1);
+            sample.next = new ListNode(2);
+            sample.next.next = new ListNode(3);
+            sample.next.next.next = new ListNode(4); 
+             tool.Solution(sample);
+        }
+    }
+}
