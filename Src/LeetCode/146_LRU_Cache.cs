6f106732858c78d10edb932a4d56ff805d1ed601
LouisGuo <2213429531@qq.com>
2016-09-01 15:35:41 +0800

    LRUCache

---
+++ Src/LeetCode/146_LRU_Cache.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LRUCache
+    {
+        private List<int> keys;
+        private Dictionary<int, int> values;
+
+
+        public LRUCache(int capacity)
+        {
+            this.keys = new List<int>(capacity);
+            this.values = new Dictionary<int, int>(capacity);
+        }
+
+        public int Get(int key)
+        {
+            if (keys.Remove(key))
+            {
+                keys.Add(key);
+                return values[key];
+            }
+            else
+                return -1;
+        }
+
+        public void Set(int key, int value)
+        {
+            if (keys.Remove(key))
+            {
+                keys.Add(key);
+                values[key] = value;
+            }
+            else
+            {
+                if (keys.Count == keys.Capacity)
+                {
+                    values.Remove(keys[0]);
+                    keys.RemoveAt(0);
+                }
+                keys.Add(key);
+                values[key] = value;
+            }
+        }
+    }
+}
