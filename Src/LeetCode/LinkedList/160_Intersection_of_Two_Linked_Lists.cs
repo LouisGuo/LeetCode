9677d1c06e694c244228608121a67879687a2674
LouisGuo <2213429531@qq.com>
2016-09-02 13:56:03 +0800

    IntersectionofTwoLinkedLists

---
+++ Src/LeetCode/LinkedList/160_Intersection_of_Two_Linked_Lists.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class IntersectionofTwoLinkedLists
+    {
+        public ListNode Solution(ListNode headA, ListNode headB)
+        {
+            var currentA = headA;
+            var currentB = headB;
+            while (currentA != currentB)
+            {
+                currentA = currentA == null ? headB : currentA.next;
+                currentB = currentB == null ? headA : currentB.next;
+            }
+            return currentA;
+        }
+
+        public ListNode Solution_Slow(ListNode headA, ListNode headB)
+        {
+            if (headA != null && headB != null)
+            {
+                var currentA = headA;
+                var currentB = headB;
+                var countA = 0;
+                var countB = 0;
+                while (currentA != null)
+                {
+                    currentA = currentA.next;
+                    countA++;
+                }
+                while (currentB != null)
+                {
+                    currentB = currentB.next;
+                    countB++;
+                }
+                currentA = headA;
+                currentB = headB;
+                if (countA > countB)
+                {
+                    for (int i = 0; i < countA - countB; i++)
+                    {
+                        currentA = currentA.next;
+                    }
+                }
+                else if (countA < countB)
+                {
+                    for (int i = 0; i < countB - countA; i++)
+                    {
+                        currentB = currentB.next;
+                    }
+                }
+                while (currentB != currentA)
+                {
+                    currentA = currentA.next;
+                    currentB = currentB.next;
+                }
+                return currentA;
+            }
+            return null;
+        }
+    }
+}
