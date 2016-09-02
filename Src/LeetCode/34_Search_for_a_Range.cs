1ff43900a0c281f8445ac71f479a98d82bed793a
LouisGuo <2213429531@qq.com>
2016-09-02 09:59:56 +0800

    SearchforaRange

---
+++ Src/LeetCode/34_Search_for_a_Range.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SearchforaRange
+    {
+        public int[] Solution_Fast(int[] nums, int target)
+        {
+            if (nums != null && nums.Length > 0 && nums[0] <= target && nums[nums.Length - 1] >= target)
+            {
+                var left = 0;
+                var right = nums.Length - 1;
+                var middle = left + (right - left) / 2;
+                while (left < right - 1)
+                {
+                    if (nums[middle] < target)
+                    {
+                        left = middle + 1;
+                    }
+                    else if (nums[middle] > target)
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        left = middle;
+                        right = middle;
+                        while (right < nums.Length && nums[right] == target)
+                        {
+                            right++;
+                        }
+                        while (left > -1 && nums[left] == target)
+                        {
+                            left--;
+                        }
+                        return new int[] { ++left, --right };
+                    }
+                    middle = left + (right - left) / 2;
+                }
+                if (nums[left] == target && nums[right] == target)
+                    return new int[] { left, right };
+                else if (nums[left] == target && nums[right] != target)
+                    return new int[] { left, left };
+                else if (nums[left] != target && nums[right] == target)
+                    return new int[] { right, right };
+                else
+                    return new int[] { -1, -1 };
+            }
+            return new int[] { -1, -1 };
+        }
+
+        public int[] Solution(int[] nums, int target)
+        {
+            if (nums != null && nums.Length > 0 && nums[0] <= target && nums[nums.Length - 1] >= target)
+            {
+                var left = 0;
+                var right = nums.Length - 1;
+                var middle = left + (right - left) / 2;
+                while (nums[left] < nums[right])
+                {
+                    if (nums[middle] < target)
+                    {
+                        left = middle + 1;
+                    }
+                    else if (nums[middle] > target)
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        if (nums[left] != target)
+                            left++;
+                        if (nums[right] != target)
+                            right--;
+                    }
+                    middle = left + (right - left) / 2;
+                }
+                if (nums[left] == target && nums[right] == target)
+                    return new int[] { left, right };
+                else
+                    return new int[] { -1, -1 };
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
---
+++ Src/Test/SearchforaRangeTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class SearchforaRangeTests
+    {
+        [TestMethod()]
+        public void Solution_FastTest()
+        {
+            var tool = new SearchforaRange();
+            var result = tool.Solution_Fast(new int[] {5,7,7,8,8,10},8);
+        }
+
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var tool = new SearchforaRange();
+            var result = tool.Solution(new int[] { 1,2,3 }, 2);
+        }
+    }
+}
