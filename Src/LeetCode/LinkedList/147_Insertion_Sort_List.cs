ae3d937cd6ecc4577d88e4868bb0f51c27d28ddb
LouisGuo <2213429531@qq.com>
2016-09-02 14:08:19 +0800

    InsertionSortList

---
+++ Src/LeetCode/LinkedList/147_Insertion_Sort_List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class InsertionSortList
+    {
+        public ListNode Solution(ListNode head)
+        {
+            if (head != null && head.next != null)
+            {
+                var current = head.next;
+                head.next = null;
+                while (current != null)
+                {
+                    var temp = current.next;
+                    if (current.val < head.val)
+                    {
+                        current.next = head;
+                        head = current;
+                    }
+                    else
+                    {
+                        var currentTemp = head.next;
+                        var pre = head;
+                        while (currentTemp != null && currentTemp.val < current.val)
+                        {
+                            pre = currentTemp;
+                            currentTemp = currentTemp.next;
+                        }
+                        pre.next = current;
+                        current.next = currentTemp;
+                    }
+                    current = temp;
+                }
+            }
+            return head;
+        }
+    }
+}
