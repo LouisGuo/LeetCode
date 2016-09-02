8b56c660d9730272f2bd9cc4faa9dcf930dfbdc3
LouisGuo <2213429531@qq.com>
2016-09-02 10:43:45 +0800

    Permutations

---
+++ Src/LeetCode/46_Permutations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Permutations
+    {
+        public IList<IList<int>> Solution(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            if (nums != null && nums.Length > 0)
+            {
+                result.Add(new List<int>() { nums[nums.Length - 1] });
+                for (int i = nums.Length - 2; i > -1; i--)
+                {
+                    GetN(result, nums[i]);
+                }
+            }
+            return result;
+        }
+
+        private void GetN(IList<IList<int>> result, int num)
+        {
+            var listLength = result[0].Count + 1;
+            var resultLength = result.Count;
+            for (int i = 1; i < listLength; i++)
+            {
+                for (int j = 0; j < resultLength; j++)
+                {
+                    var temp = new List<int>();
+                    for (int inner = 0; inner < listLength - 1; inner++)
+                    {
+                        temp.Add(result[j][inner]);
+                    }
+                    temp.Insert(i, num);
+                    result.Add(temp);
+                }
+            }
+            for (int i = 0; i < resultLength; i++)
+            {
+                result[i].Insert(0, num);
+            }
+        }
+    }
+}
---
+++ Src/Test/PermutationsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class PermutationsTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var tool = new Permutations();
+            var result = tool.Solution(new int[] { 1,2});
+        }
+    }
+}
