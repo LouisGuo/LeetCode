088570b5866af260f9cf3aac8728db0fa0489960
LouisGuo <2213429531@qq.com>
2016-09-03 12:15:55 +0800

    PlusOne

---
+++ Src/LeetCode/66_Plus_One.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class PlusOne
+    {
+        public int[] Solution(int[] digits)
+        {
+            if (digits != null && digits.Length > 0)
+            {
+                var result = new int[digits.Length];
+                var lastIncrease = 1;
+                for (int i = digits.Length - 1; i >= 0; i--)
+                {
+                    var sum = digits[i];
+                    if (lastIncrease > 0)
+                    {
+                        sum += lastIncrease;
+                        lastIncrease = sum / 10;
+                        sum %= 10;
+                    }
+                    result[i] = sum;
+                }
+                if (lastIncrease > 0)
+                {
+                    result = new int[digits.Length + 1];
+                    result[0] = 1;
+                }
+                return result;
+            }
+            return null;
+        }
+    }
+}
