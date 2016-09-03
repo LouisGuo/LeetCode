a110dc3a1fcdbd8e3c5c129156da733c7a8d9773
LouisGuo <2213429531@qq.com>
2016-09-03 14:50:11 +0800

    RotateImage

---
+++ Src/LeetCode/48_Rotate_Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RotateImage
+    {
+        public void Rotate(int[,] matrix)
+        {
+            var length = matrix.GetLength(0);
+            var copy = new int[length, length];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    copy[i, j] = matrix[i, j];
+                }
+            }
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    matrix[j, length - i - 1] = copy[i, j];
+                }
+            }
+        }
+    }
+}
