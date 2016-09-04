4709139652098dc94f50a35866673dcafa94d19d
LouisGuo <2213429531@qq.com>
2016-09-04 19:05:47 +0800

    IntersectionofTwoArrays

---
+++ Src/LeetCode/349_Intersection_of_Two_Arrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class IntersectionofTwoArrays
+    {
+        public int[] Solution(int[] nums1, int[] nums2)
+        {
+            return nums1.Intersect(nums2).Distinct().ToArray();
+        }
+    }
+}
