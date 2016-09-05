ae2d31b20d116589cd6d342976a590e85a03f702
LouisGuo <2213429531@qq.com>
2016-09-05 20:52:31 +0800

    BulbSwitcher

---
+++ Src/LeetCode/319_Bulb_Switcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class BulbSwitcher
+    {
+        public int Solution(int n)
+        {
+            var result = 0;
+            if (n > 0)
+            {
+                var flags = new BitArray(n);
+                flags.SetAll(true);
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = i; j < n; j += i + 1)
+                    {
+                        flags[j] = !flags[j];
+                    }
+                }
+                foreach (bool flag in flags)
+                {
+                    if (flag)
+                        result++;
+                }
+            }
+            return result;
+        }
+
+        public int Solution_Fast(int n)
+        {
+            return (int)Math.Sqrt(n);
+        }
+        }
+}
---
+++ Src/Test/BulbSwitcherTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class BulbSwitcherTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var tool = new BulbSwitcher();
+            var result = tool.Solution(9999999);
+        }
+    }
+}
