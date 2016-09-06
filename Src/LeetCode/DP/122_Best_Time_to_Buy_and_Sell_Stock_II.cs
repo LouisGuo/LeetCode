3735490aaf824adb112b534cbd72857d42843c82
LouisGuo <2213429531@qq.com>
2016-09-06 20:51:33 +0800

    BestTimetoBuyandSellStockII

---
+++ Src/LeetCode/DP/122_Best_Time_to_Buy_and_Sell_Stock_II.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class BestTimetoBuyandSellStockII
+    {
+        public int Solution(int[] prices)
+        {
+            var result = 0;
+            if (prices != null && prices.Length > 1)
+            {
+                for (int i = 1; i < prices.Length; i++)
+                {
+                    var added = prices[i] - prices[i - 1];
+                    if (added > 0)
+                        result += added;
+                }
+            }
+            return result;
+        }
+    }
+}
