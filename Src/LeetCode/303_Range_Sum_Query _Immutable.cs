ce5ec27644dac0c6b15905c574471d981a6732dc
LouisGuo <2213429531@qq.com>
2016-09-06 16:47:11 +0800

    303_Range_Sum_Query _Immutable

---
+++ Src/LeetCode/303_Range_Sum_Query _Immutable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class NumArray
+    {
+        private int[] sums;
+
+        public NumArray(int[] nums)
+        {
+            if (nums != null && nums.Length > 0)
+            {
+                this.sums = new int[nums.Length];
+                sums[0] = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    sums[i] = sums[i - 1] + nums[i];
+                }
+            }
+        }
+
+        public int SumRange(int i, int j)
+        {
+            var result = 0;
+            if (i == 0)
+                result = sums[j];
+            else
+                result = sums[j] - sums[i - 1];
+            return result;
+        }
+    }
+}
