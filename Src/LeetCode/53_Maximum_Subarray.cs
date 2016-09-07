77d4bcc395630a6fd1d2abde8cf0df9a830ba480
LouisGuo <2213429531@qq.com>
2016-09-07 14:54:55 +0800

    MaximumSubarray

--- Src/LeetCode/53_Maximum_Subarray.cs
+++ Src/LeetCode/53_Maximum_Subarray.cs
@@ -11,7 +11,16 @@ namespace LeetCode
         public int Solution(int[] nums)
         {
             var result = 0;
-            
+            if (nums != null && nums.Length > 0)
+            {
+                result = nums[0];
+                var fn_1 = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    fn_1 = Math.Max(nums[i], fn_1 + nums[i]);
+                    result = Math.Max(fn_1, result);
+                }
+            }
             return result;
         }
     }
