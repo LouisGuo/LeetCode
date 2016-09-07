2fbf84535e2eacf532f256c26b737777d202a05a
LouisGuo <2213429531@qq.com>
2016-09-07 15:27:01 +0800

    MaximumProductSubarray

---
+++ Src/LeetCode/DP/152_Maximum_Product_Subarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MaximumProductSubarray
+    {
+        public int Solution(int[] nums)
+        {
+            var result = 0;
+            if (nums != null && nums.Length > 0)
+            {
+                result = nums[0];
+                var maxn_1 = result;
+                var minn_1 = result;
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    var max = maxn_1 * nums[i];
+                    var min = minn_1 * nums[i];
+                    maxn_1 = Math.Max(nums[i], Math.Max(max, min));
+                    minn_1 = Math.Min(nums[i], Math.Min(max, min));
+                    result = Math.Max(result, maxn_1);
+                }
+            }
+            return result;
+        }
+    }
+}
