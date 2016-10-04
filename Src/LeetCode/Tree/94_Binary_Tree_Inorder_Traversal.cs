694c04d31374bb2086197917e8b8ed0f75753dfb
LouisGuo <2213429531@qq.com>
2016-10-04 21:02:01 +0800

    BinaryTreeInorderTraversal

---
+++ Src/LeetCode/Tree/94_Binary_Tree_Inorder_Traversal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> Solution(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root != null)
+            {
+                result.Add(root.val);
+                if (root.left != null)
+                    result.InsertRange(0, Solution(root.left));
+                if (root.right != null)
+                    result.AddRange(Solution(root.right));
+            }
+            return result;
+        }
+    }
+}
