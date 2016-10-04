5008dcd73c33a41172ebbe26cd090cfb499ca678
LouisGuo <2213429531@qq.com>
2016-10-04 21:55:05 +0800

    MinimumDepthofBinaryTree

---
+++ Src/LeetCode/Tree/111_Minimum_Depth_of_Binary_Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MinimumDepthofBinaryTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            if (root.left == null && root.right == null)
+                return 1;
+            else if (root.left != null && root.right == null)
+            {
+                return 1 + MinDepth(root.left);
+            }
+            else if (root.left == null && root.right != null)
+            {
+                return 1 + MinDepth(root.right);
+            }
+            else
+                return 1 + Math.Min(MinDepth(root.left), MinDepth(root.right));
+        }
+    }
+}
