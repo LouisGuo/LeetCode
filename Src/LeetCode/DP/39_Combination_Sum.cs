acda58878d3a002a652abcca9a93d3b841cf1dfe
LouisGuo <2213429531@qq.com>
2016-10-11 22:31:22 +0800

    CombinationSum draft

---
+++ Src/LeetCode/DP/39_Combination_Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class CombinationSum
+    {
+        public IList<IList<int>> Solution(int[] candidates, int target)
+        {
+            var result = new List<IList<IList<int>>>();
+
+            return result[target - 1];
+        }
+    }
+}
