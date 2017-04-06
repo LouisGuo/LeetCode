9978f066e0ce1135b68ad278299c0cc5c8a1803c
LouisGuo <2213429531@qq.com>
2017-04-06 15:54:37 +0800

    _412_Fizz_Buzz

---
+++ Src/LeetCode/412_Fizz_Buzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _412_Fizz_Buzz
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            var result = new List<String>();
+            for (int i = 1; i < n + 1; i++)
+            {
+                result.Add(i.ToString());
+            }
+            for (int i = 1; i * 3 <= n; i++)
+            {
+                result[i * 3 - 1] = "Fizz";
+            }
+            for (int i = 1; i * 5 <= n; i++)
+            {
+                if (result[i * 5 - 1].Equals("Fizz"))
+                {
+                    result[i * 5 - 1] = "FizzBuzz";
+                }
+                else
+                {
+                    result[i * 5 - 1] = "Buzz";
+                }
+            }
+            return result;
+        }
+    }
+}
